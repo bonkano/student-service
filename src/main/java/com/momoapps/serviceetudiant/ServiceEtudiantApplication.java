2cea67a82974604b93064d2ac5549a51d52777c1
Mohamed Maiga <moha12792@gmail.com>
2018-07-24 19:59:05 +0000

    Initial commit

---
+++ src/main/java/com/momoapps/serviceetudiant/ServiceEtudiantApplication.java
@@ -0,0 +1,12 @@
+package com.momoapps.serviceetudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEtudiantApplication.class, args);
+	}
+}
---
+++ src/test/java/com/momoapps/serviceetudiant/ServiceEtudiantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.momoapps.serviceetudiant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceEtudiantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
